05647e79d2c443ed01f9c9b7667c77e5487446ce
Sevostyanov_AV <SevostyanovAV@cbr.ru>
2025-02-13 14:30:10 +0300

    Part 4 - add css style

--- src/main/java/ch/makery/MainApp.java
+++ src/main/java/ch/makery/MainApp.java
@@ -8,6 +8,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -50,6 +51,9 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         this.primaryStage = primaryStage;
+
+        // Устанавливаем иконку приложения.
+        this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
         initRootLayout();
         showPersonOverview();
     }
